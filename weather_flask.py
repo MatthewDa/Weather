2127d30ed3a81fa549b41e2d8a8a4f2f9ab25626
tanveerm176 <tanveerm176@gmail.com>
2015-07-21 14:44:23 -0400

    idk

--- weather_flask.py
+++ weather_flask.py
@@ -8,6 +8,38 @@ weather_flask=Flask(__name__)
 def root():
     return render_template('main.html')
 
+
+
+
+
+
+@weather_flask.route('/weather')
+def weather_city():
+    return render_template('Azkaban.html')
+    # if category == 'Malfoy Manor':
+
+    # if category == 'Shrieking Shack':
+
+    # if category == 'Diagon Alley':
+
+    # if category == 'Hogwarts':
+
+    # if category == 'Azkaban':
+   
+
+
+
+
 if __name__ == '__main__':
     weather_flask.debug = True
     weather_flask.run(host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
